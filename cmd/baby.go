0070f7f69e01b2537ae679e73c85a52a4246623e
BuTn <kimmosc2@163.com>
2019-09-15 16:35:03 +0800

    pretty code

--- cmd/baby.go
+++ cmd/baby.go
@@ -7,7 +7,7 @@ import (
 	"log"
 )
 
-//client:%d   times:%d   url:%s
+// client:%d   times:%d   url:%s
 var infoTemplate = "Baby-run:a simple benchmark tool.\n" +
 	"client quantities:%d\n" +
 	"preset times:%d seconds\n" +
@@ -23,10 +23,10 @@ func main() {
 
 	run.Start(config)
 
-	//duration, err := run.Get(config.Url)
-	//if err != nil {
+	// duration, err := run.Get(config.Url)
+	// if err != nil {
 	//	log.Fatal("request error:", err)
-	//}
-	//fmt.Printf("test complete,use duration:%vms\n", duration.Nanoseconds()/1e6)
-	//fmt.Printf(infoTemplate, config.Client, config.Times, config.Url)
+	// }
+	// fmt.Printf("test complete,use duration:%vms\n", duration.Nanoseconds()/1e6)
+	// fmt.Printf(infoTemplate, config.Client, config.Times, config.Url)
 }
